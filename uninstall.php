8774e86ed75368fccd24deb52fe8cd4014aa1ab6
Te Taipo <hokioi-security@protonmail.ch>
2018-07-27 20:57:54 +1200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * uninstall version 2.1.1
+ * uninstall version 2.1.2
  */
 require_once( 'pareto_functions.php' );
 $ParetoSecurity = new pareto_functions();
@@ -9,16 +9,15 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ) || !WP_UNINSTALL_PLUGIN || dirname( WP_UN
 }
 if ( !is_user_logged_in() ) {
     wp_die( 'You must be logged in to run this script.' );
-	$ParetoSecurity->send444();
+	$ParetoSecurity->send403();
 }
 if ( !current_user_can( 'install_plugins' ) ) {
     wp_die( 'You do not have permission to run this script.' );
-	$ParetoSecurity->send444();
+	$ParetoSecurity->send403();
 }
 
 if ( false !== $ParetoSecurity->is_wp( true ) ) {
     if ( false !== $ParetoSecurity->get_file_perms( $ParetoSecurity->htapath(), true, true ) )
         $ParetoSecurity->htaccess_unbanip();
 } else
-    $ParetoSecurity->send444();
-?>
+    $ParetoSecurity->send403();
