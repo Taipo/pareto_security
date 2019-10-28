f7f18838d9979f377ca8047e0d88a30850730a7b
Te Taipo <hokioi-security@protonmail.ch>
2019-10-28 22:57:22 +1300

    Update hokioi.js

--- js/hokioi.js
+++ js/hokioi.js
@@ -1,8 +1,18 @@
-function check() {
-    document.getElementById("pareto_security_settings_options[hard_ban_mode]").checked = true;
+function checkAll(formname, checktoggle) {
+  checkboxes = document[formname].getElementsByTagName('input');
+  for (var i=0; i<checkboxes.length; i++)  {
+    if (checkboxes[i].name != "multiselect" && checkboxes[i].name.search("security_settings_options") <= 0) {
+        if (checkboxes[i].type == 'checkbox' && checkboxes[i].checked == false ) {
+            checkboxes[i].checked = checktoggle;
+        } else {
+            checkboxes[i].checked = false;
+        }
+    }
+  }
 }
 function uncheck() {
     document.getElementById("pareto_security_settings_options[hard_ban_mode]").checked = false;
+    document.getElementById("pareto_security_settings_options[tor_block]").checked = false;
 }
 function checkRow(chkrow, thisrow) {
     if (typeof(chkrow) == "object") {
