f9a38ed2bf09bb7afb2abc6b254a652cce76dd1c
Te Taipo <hokioi-security@protonmail.ch>
2021-12-13 23:10:00 +1300

    Add files via upload

---
+++ lib/pareto_detect_log4shell.php
@@ -0,0 +1,34 @@
+<?php
+class Logjam_Filter {
+    
+    public static function logjam_check( $input = '' ): bool {
+        # Attempts to exploit CVE-2021-44228
+        # Standard
+        # - ${jndi:ldap://,${jndi:rmi://,${jndi:ldaps://,${jndi:dns://
+        if ( false !== strpos( $input, 'jndi' ) ) {
+            if ( false !== strpos( $input, 'jndi:ldap' ) ||
+                 false !== strpos( $input, 'jndi:rmip' ) ||
+                 false !== strpos( $input, 'jndi:ldaps' ) ||
+                 false !== strpos( $input, 'jndi:dns' ) ) {
+                 return true;
+            }
+        }
+        # Deal with variants          
+        if ( substr_count( $input, '$' ) >= 4 &&
+             substr_count( $input, '{' ) >= 4 &&
+             substr_count( $input, '}' ) >= 4 &&
+             substr_count( $input, ':' ) >= 4 ) {
+             $list = array( 'lower', 'upper' );
+             $input = str_replace( $list, '', $input );
+             $input = preg_replace( "/[^a-zA-Z]/i", "", $input );
+             if ( false !== strpos( $input, 'ldap' ) ||
+                  false !== strpos( $input, 'rmip' ) ||
+                  false !== strpos( $input, 'ldaps' ) ||
+                  false !== strpos( $input, 'dns' ) ) {
+                  return true;
+            }
+        }
+        return false;
+    }
+}
+?>
