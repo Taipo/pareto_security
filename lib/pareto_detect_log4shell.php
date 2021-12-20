bbb28f66dff07a941bfbf9b5ff1b5a0296e179a8
Te Taipo <hokioi-security@protonmail.ch>
2021-12-20 23:26:07 +1300

    Update pareto_detect_log4shell.php

--- lib/pareto_detect_log4shell.php
+++ lib/pareto_detect_log4shell.php
@@ -4,7 +4,6 @@ class Logjam_Filter {
     public static function logjam_check( $input = '' ): bool {
         # Attempts to exploit CVE-2021-44228
         # Standard
-        # - ${jndi:ldap://,${jndi:rmi://,${jndi:ldaps://,${jndi:dns://
         if ( false !== strpos( $input, 'jndi' ) ) {
             if ( false !== strpos( $input, 'jndi:ldap' ) ||
                  false !== strpos( $input, 'jndi:rmip' ) ||
@@ -13,17 +12,18 @@ class Logjam_Filter {
                  return true;
             }
         }
-        # Deal with variants          
-        if ( substr_count( $input, '$' ) >= 4 &&
-             substr_count( $input, '{' ) >= 4 &&
-             substr_count( $input, '}' ) >= 4 &&
-             substr_count( $input, ':' ) >= 4 ) {
+        # Deal with variants
+        if ( substr_count( $input, '$' ) >= 2 &&
+             substr_count( $input, '{' ) >= 2 &&
+             substr_count( $input, '}' ) >= 2 &&
+             substr_count( $input, ':' ) >= 3 ) {
              $list = array( 'lower', 'upper' );
              $input = str_replace( $list, '', $input );
              $input = preg_replace( "/[^a-zA-Z]/i", "", $input );
              if ( false !== strpos( $input, 'ldap' ) ||
                   false !== strpos( $input, 'rmip' ) ||
                   false !== strpos( $input, 'ldaps' ) ||
+                  false !== strpos( $input, 'jndi' ) ||
                   false !== strpos( $input, 'dns' ) ) {
                   return true;
             }
