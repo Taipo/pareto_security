b2b00cf54d5e005cb2bcd6cd29eb7dcd12421d12
Te Taipo <hokioi-security@protonmail.ch>
2025-10-24 18:53:49 +1300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,22 +2,21 @@
 /**
  * uninstall
  */
-require_once( 'pareto_functions.php' );
+require_once ('pareto_functions.php');
 $ParetoSecurity = new pareto_functions();
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) || !WP_UNINSTALL_PLUGIN || dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) ) ) {
+if (!defined('WP_UNINSTALL_PLUGIN') || !WP_UNINSTALL_PLUGIN || dirname(WP_UNINSTALL_PLUGIN) != dirname(plugin_basename(__FILE__))) {
     $ParetoSecurity->send444();
 }
-if ( !is_user_logged_in() ) {
-    wp_die( 'You must be logged in to run this script.' );
-	$ParetoSecurity->send403();
+if (!is_user_logged_in()) {
+    wp_die('You must be logged in to run this script.');
+    $ParetoSecurity->send403();
 }
-if ( !current_user_can( 'install_plugins' ) ) {
-    wp_die( 'You do not have permission to run this script.' );
-	$ParetoSecurity->send403();
+if (!current_user_can('install_plugins')) {
+    wp_die('You do not have permission to run this script.');
+    $ParetoSecurity->send403();
 }
 
-if ( false !== $ParetoSecurity->is_wp( true ) ) {
-    if ( false !== $ParetoSecurity->get_file_perms( $ParetoSecurity->htapath(), true, true ) )
-        $ParetoSecurity->htaccess_unbanip();
-} else
-    $ParetoSecurity->send403();
+if (false !== $ParetoSecurity->is_wp(true)) {
+    if (false !== $ParetoSecurity->get_file_perms($ParetoSecurity->htapath() , true, true)) $ParetoSecurity->htaccess_unbanip();
+}
+else $ParetoSecurity->send403();
